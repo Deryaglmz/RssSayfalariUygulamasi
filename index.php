d76551c9005588681a0941fecfef6dce185acebb
Derya Gülmez <deryaglmz0@gmail.com>
2024-06-28 11:56:29 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+    header("Content-Type: text/xmlns");
+    try {
+        $VeritabaniBaglantisi = new PDO("mysql:host=localhost;dbname=urun;charset=UTF8", "root", "9900");
+    } catch(PDOException $Hata) {
+        echo $Hata->getMessage();
+        die();
+    }
+
+    echo "
+    <?xml version='1.0' encoding='UTF-8'?>
+    <rss xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' version='2.0'>
+        <channel>
+            <title>Extra Eğitim</title>
+            <description>İleri Seviye Eğitim Setleri</description>
+            <link>http://www.extraegitim.com</link>
+            <language>tr</language>
+    ";
+
+    $Sorgu = $VeritabaniBaglantisi->prepare("SELECT * FROM urunler");
+    $Sorgu->execute();
+    $SorguSayisi = $Sorgu->rowCount();
+    $SorguKayitlari = $Sorgu->fetchAll(PDO::FETCH_ASSOC);
+
+    if ($SorguSayisi > 0) {
+        foreach ($SorguKayitlari as $Satirlar) {
+            $UrunAdi = $Satirlar["urunadi"];
+            $UrunFiyati = $Satirlar["urunfiyati"];
+            
+            echo "
+            <item>
+                <title>$UrunAdi</title>
+                <price>$UrunFiyati</price>
+            </item>
+            ";
+        }
+    }
+
+    echo "
+        </channel>
+    </rss>
+    ";
+    $VeritabaniBaglantisi = null;
+?>
